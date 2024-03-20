05c2906531490611c174f00fa74172a1a888ec2d
truongghieu <chippstudio@gmail.com>
2024-03-20 10:22:29 +0700

    cc

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ from scripts.boss.greenskin import greenskin
 from scripts.card import card,card_score,card_damage,card_zombie,card_manager
 from scripts.button import button
 from scripts.effect import effect
-from scripts.player import Player
+from scripts.player import Player 
 
 font_dat = {'A':[3],'B':[3],'C':[3],'D':[3],'E':[3],'F':[3],'G':[3],'H':[3],'I':[3],'J':[3],'K':[3],'L':[3],'M':[5],'N':[3],'O':[3],'P':[3],'Q':[3],'R':[3],'S':[3],'T':[3],'U':[3],'V':[3],'W':[5],'X':[3],'Y':[3],'Z':[3],
           'a':[3],'b':[3],'c':[3],'d':[3],'e':[3],'f':[3],'g':[3],'h':[3],'i':[1],'j':[2],'k':[3],'l':[3],'m':[5],'n':[3],'o':[3],'p':[3],'q':[3],'r':[2],'s':[3],'t':[3],'u':[3],'v':[3],'w':[5],'x':[3],'y':[3],'z':[3],
@@ -30,6 +30,7 @@ font_dat = {'A':[3],'B':[3],'C':[3],'D':[3],'E':[3],'F':[3],'G':[3],'H':[3],'I':
 
 class Game:
     def __init__(self):
+        # cc
         # set up for pygame
         pygame.init()
         pygame.display.set_caption('Zombie Killer')
