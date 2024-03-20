54f186104f9df988730c0e0aca0debdaad84cffa
truongghieu <chippstudio@gmail.com>
2024-03-20 10:13:55 +0700

    cc

--- Scripts/system/enum.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class game_state(enum.Enum):
-    menu = 0
-    gameplay = 1
-    
--- Scripts/system/sound.py
+++
@@ -1,6 +0,0 @@
-import pygame
-def load_snd(name):
-    return pygame.mixer.Sound('data/sfx/' + name + '.wav')
-
-def load_kill(name):
-    return pygame.mixer.Sound('data/sfx/kill/' + name +'.WAV')
